57e8bb07d9c4718686853ae309c28cc5b5d238ea
Lukas Gabsi <lukas.gabsi@towa.at>
2022-12-03 15:42:08 +0100

    roids

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <title>Gabsi's Advent Calendar</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Gabsi's Advent Calendar" />
+        <meta name="author" content="Lukas Gabsi" />
+        <meta name="keywords" content="advent, calendar, gabsi" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`bg-black w-full ${mountainsOfChristmas.variable}`}>
         <div className="container mx-auto px-4">
